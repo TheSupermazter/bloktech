388a287f9e4b1f28b7c936e15a5cba8ea0f12a06
Thomas Norden <supermas2000@hotmail.nl>
2024-02-06 16:48:30 +0100

    nieuwe opdrachten erin

---
+++ opdrachtenLes/PT007.js
@@ -0,0 +1,26 @@
+/* Met de toetscombinatie [CMD] + [/] kun je '//' voor de regel weghalen en terugplaatsen */
+
+/* OPDRACHT 1: Definieer een object */
+// Definieer een eenvoudig object met de naam persoon dat de eigenschappen naam en leeftijd heeft. Geef het object de waarden "John" voor naam en 30 voor leeftijd.
+
+
+
+/* OPDRACHT 2: Uitbreiding */
+// Breid het object persoon uit met verschillende soorten eigenschappen, zoals een string, een numerieke waarde, een boolean, en een array.
+
+
+
+/* OPDRACHT 3: Dot & bracket notation */
+// Gebruik zowel dot notatie als bracket notatie om de waarde van de naam-eigenschap van het persoon-object op te halen en naar de console te loggen.
+
+
+
+/* OPDRACHT 4: Methode */
+// Voeg een methode verjaardagVieren toe aan het persoon-object. Deze methode moet de leeftijd van de persoon met één verhogen. Je kan binnen het object naar de eigenschap leeftijd verwijzen met "this.leeftijd".
+
+
+
+/* OPDRACHT 5: Boodschap loggen */
+// Breid de methode verjaardagVieren uit om een bericht naar de console te loggen waarin de naam en nieuwe leeftijd van de persoon worden weergegeven.
+
+
---
+++ opdrachtenLes/PT008.js
@@ -0,0 +1,48 @@
+/* Met de toetscombinatie [CMD] + [/] kun je '//' voor de regel weghalen en terugplaatsen */
+
+/* OPDRACHT 1: For loop - Tel tot 5 */
+// Schrijf een for-loop die de getallen van 1 tot 5 logt naar de console.
+
+
+
+/* OPDRACHT 2: For loop - Maak de tafel van 3 */
+// Schrijf een for-loop die de tafel van 3 logt naar de console (van 3 tot 30).
+
+
+
+/* OPDRACHT 3: While loop */
+// Programmmer de herhalingen van opdracht 1 met een while loop
+
+
+
+/* OPDRACHT 4: While loop */
+// Programmmer de herhalingen van opdracht 2 met een while loop
+
+
+
+/* OPDRACHT 5: For ... of loop - Log kleuren */
+// Gegeven een array met kleuren (['rood', 'groen', 'blauw']), schrijf een for...of-loop om elke kleur naar de console te loggen.
+
+// let kleuren = ['rood', 'groen', 'blauw']
+
+
+
+/* OPDRACHT 6: For ... of loop - Lengte van elk woord */
+// Gegeven een array met woorden, schrijf een for...of-loop om de lengte van elk woord naar de console te loggen.
+
+// let woorden = ['appel', 'banaan', 'kiwi']
+
+
+
+/* OPDRACHT 7: For ... in loop - Log Eigenschappen */
+// Gegeven een object met eigenschappen, schrijf een for...in-loop om elke eigenschap naar de console te loggen.
+
+// let persoon = { naam: 'John', leeftijd: 25, stad: 'Amsterdam' }
+
+
+
+/* OPDRACHT 8: For ... in loop - Log waarden */
+// Gegeven een object met eigenschappen, schrijf een for...in-loop om elke eigenschap naar de console te loggen.
+
+// let telefoon = { merk: 'Samsung', model: 'Galaxy', jaar: 2022 }
+
