57edc3d58fe74b7c2ad6f501493ca26f961a5dec
Thomas Norden <supermas2000@hotmail.nl>
2024-02-06 16:50:41 +0100

    PT009 toegevoegd

---
+++ opdrachtenLes/PT009.js
@@ -0,0 +1,18 @@
+/* STAP 1 */ 
+// Maak een .json bestand met daarin enkele gegevens (studenten, boeken, films, etc) 
+// Zorg dat het een geldige JSON-structuur heeft (zie voorbeeld)
+
+
+/* STAP 2 */ 
+// Maak een javascript-bestand waarin je gebruik maakt van de functie fetch om het JSON-bestand in te lezen
+// Log met behulp van een loop de gegevens in het bestand naar de console (tip: forEach)
+
+
+/* Stap 3 */
+// Maak een boilerplate html-bestand die je een <h1> en een lege <ul> heeft
+// Pas de iteratie met de loop in het script aan zodat er voor elk opgehaald gegeven een nieuwe li in een ul wordt aangemaakt met daarin de data
+// TIPS
+// const ulElement = document.querySelector('ul')
+// data.forEach( item => {} )
+// const liElement = document.createElement('li')
+// ulElement.appendChild(liElement)
